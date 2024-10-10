4f3553271c7a236fa240db70bacce36b53e495c4
Chang Chen <baibaichen@gmail.com>
2024-10-10 13:49:03 +0800

    Optimize performance with lazy projection to avoid reading unused columns #45868
    (cherry picked from commit f05319100a559b3cd305d55efff764d629e071b8)

--- src/DataTypes/Serializations/SerializationObject.h
+++ src/DataTypes/Serializations/SerializationObject.h
@@ -62,6 +62,7 @@ public:
 
     void deserializeBinaryBulkWithMultipleStreams(
         ColumnPtr & column,
+        size_t rows_offset,
         size_t limit,
         DeserializeBinaryBulkSettings & settings,
         DeserializeBinaryBulkStatePtr & state,
