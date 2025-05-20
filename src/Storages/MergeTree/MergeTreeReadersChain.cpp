5dcac93bef89f45ae02d1049784e46ec681259e4
Chang chen <changchen@apache.com>
2025-05-20 10:11:07 +0800

    [TMP] LOG

--- src/Storages/MergeTree/MergeTreeReadersChain.cpp
+++ src/Storages/MergeTree/MergeTreeReadersChain.cpp
@@ -73,10 +73,13 @@ MergeTreeReadersChain::ReadResult MergeTreeReadersChain::read(size_t max_rows, M
         return ReadResult{log};
 
     auto & first_reader = range_readers.front();
+
+    Stopwatch watch;
     auto read_result = first_reader.startReadingChain(max_rows, ranges);
     read_result.addNumBytesRead(getTotalBytesInColumns(read_result.columns));
+    LOG_TEST(log, "First reader returned: {}, time cost(ns): {}", read_result.num_rows, watch.elapsed());
 
-    LOG_TEST(log, "First reader returned: {}, requested columns: {}", read_result.dumpInfo(), first_reader.getSampleBlock().dumpNames());
+    // LOG_TEST(log, "First reader returned: {}, requested columns: {}", read_result.dumpInfo(), first_reader.getSampleBlock().dumpNames());
 
     if (read_result.num_rows != 0)
     {
