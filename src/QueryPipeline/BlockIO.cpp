2d7d8a359abc9eff53ea3715465c8dc09d8e47f7
Chang chen <changchen@apache.com>
2025-05-21 14:41:08 +0800

    dumpProcessor

--- src/QueryPipeline/BlockIO.cpp
+++ src/QueryPipeline/BlockIO.cpp
@@ -1,5 +1,8 @@
-#include <QueryPipeline/BlockIO.h>
 #include <Interpreters/ProcessList.h>
+#include <Processors/Port.h>
+#include <QueryPipeline/BlockIO.h>
+#include <QueryPipeline/printPipeline.h>
+#include <Common/logger_useful.h>
 
 namespace DB
 {
@@ -46,8 +49,33 @@ BlockIO::~BlockIO()
     reset();
 }
 
+
+static std::string dumpProcessor(const Processors & processors)
+{
+    for (const auto & processor : processors)
+    {
+        WriteBufferFromOwnString buffer;
+        auto data_stats = processor->getProcessorDataStats();
+        buffer << "(";
+        buffer << "\nexecution time: " << processor->getElapsedNs() / 1000U << " us.";
+        buffer << "\ninput wait time: " << processor->getInputWaitElapsedNs() / 1000U << " us.";
+        buffer << "\noutput wait time: " << processor->getOutputWaitElapsedNs() / 1000U << " us.";
+        buffer << "\ninput rows: " << data_stats.input_rows;
+        buffer << "\ninput bytes: " << data_stats.input_bytes;
+        buffer << "\noutput rows: " << data_stats.output_rows;
+        buffer << "\noutput bytes: " << data_stats.output_bytes;
+        buffer << ")";
+        processor->setDescription(buffer.str());
+    }
+    WriteBufferFromOwnString out;
+    printPipeline(processors, out);
+    return out.str();
+}
+
 void BlockIO::onFinish(std::chrono::system_clock::time_point finish_time)
 {
+    LOG_TEST(&Poco::Logger::get("BlockIO"), "Pipeline finished: \n{}", dumpProcessor(pipeline.getProcessors()));
+
     if (finish_callback)
         finish_callback(std::move(pipeline), finish_time);
     else
